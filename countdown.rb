643354ecf8ecd2f8840a9640042884e3a8d67e02
KG ZZ <czheng5h88@gmail.com>
2019-09-06 11:00:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,17 @@
 
 def countdown(number)
   until number == 0
-    puts "#{number} SECONDS(S)!"
+    puts "#{number} SECOND(S)!"
     number = number - 1
+  end 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  until number == 0
+    puts "#{number} SECOND(S)!"
+    number = number - 1
+    sleep(1)
   end 
   return "HAPPY NEW YEAR!"
 end
